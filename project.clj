4eb1d8e8eeb9beaf1f0824aad79f566ae472b8dd
Yannick Schutz <yannick.schutz@gmail.com>
2013-09-07 01:23:33 +0200

    First try with clojure

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject web "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.taoensso/carmine "2.2.0"]
+                 [compojure "1.1.5"]
+                 [http-kit "2.1.10"]]
+  :main web.core)
---
+++ src/web/core.clj
@@ -0,0 +1,54 @@
+(ns web.core
+  (:gen-class)
+  (:require [taoensso.carmine :as car :refer (wcar)]))
+
+(use '[compojure.route :only [files not-found]]
+     '[compojure.handler :only [site]] ; form, query params decode; cookie; session, etc
+     '[compojure.core :only [defroutes GET POST DELETE ANY context]]
+     'org.httpkit.server)
+
+(def server1-conn {:pool {} :spec {}})
+(defmacro wcar* [& body] `(car/wcar server1-conn ~@body))
+
+(defn get-url [key]
+  (wcar* (car/hget "short.urls" key))
+)
+
+(defn add-url [key url]
+ (wcar* (car/hset "short.urls" key url))
+)
+
+(defn delete-url [key]
+  (wcar* (car/hdel "short.urls" key))
+)
+
+(defn redirect-to-full-url [req]
+  (let [key (-> req :params :key)]
+    {:status 301
+     :headers {"Location" (get-url key)}}
+  )
+)
+
+(defn remove-key [req]
+  (let [key (-> req :params :key)]
+    {:status 200
+     :headers { "Content-Type" "application/json" }
+     :body (delete-url key)}
+  )
+)
+
+(defn create-new-link [req]
+  (let [url (-> req :params :url) key (-> req :params :key)]
+    (add-url key url)
+    {:status 200
+    :headers { "Content-Type" "application/json" }
+    :body (str "{" key ": " url "}") }
+  )
+)
+
+(defroutes all-routes
+  (GET "/:key" [] redirect-to-full-url)
+  (DELETE "/:key" [] remove-key)
+  (POST "/" [] create-new-link))
+
+(run-server (site #'all-routes) {:port 8089})
